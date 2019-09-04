5183c69954a6d4417125088078d79a4aef547574
cvassia <45734757+cvassia@users.noreply.github.com>
2019-09-04 22:09:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let button = document.getElementById("searchBtn");
 
 function searchWeather(city) {
 
-    const proxy = "https://cors-anywhere.herokuapp.com";
+    const proxy = "https://cors-anywhere.herokuapp.com/";
     fetch(`${proxy}http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${appId}&units=${units}`).then(result => {
         return result.json();
     }).then(result => {
